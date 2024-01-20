7a14b90eed83e876674ea6d2fb9f93edf80a817b
Wildan <wildanalifrio@gmail.com>
2024-01-20 19:57:00 +0700

    Initial Commit

---
+++ dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dice
+//
+//  Created by Wildan on 20/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by Wildan on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
